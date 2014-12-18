303d488691df91e8ed236145c7368795b14194d4
hiaselhans <simon@booya.at>
2014-12-19 00:18:19 +0100

    (really) using the css

--- js/main.js
+++ js/main.js
@@ -4,17 +4,28 @@ jQuery.noConflict();
 +function($){
 function PageSwitcher($pages, options){
     var DEFAULTS = {
-        transition_time: 200,
+        transition_time: 400,
         delay: 3000
     },
         _this = this;
 
+    $pages.each(function(i, item){
+            //set an id if not present
+
+            //set css next (fade_in)
+
+            //set css active
+
+            //set css last (fade_out)
+        }
+    );
+
     this.in_a_transition = false;
     this.$pages = $pages;
-    this.options = jQuery.extend(DEFAULTS, options);
+    this.options = $.extend(DEFAULTS, options);
     this.current = 0;
     this.page_no = $pages.length;
-    this.transitions = $pages.map(function() {
+    this.transitions = $pages.map(function GetTransitions() {
         var $page = $(this);
         return {
             type: $page.attr('transition'),
@@ -22,24 +33,18 @@ function PageSwitcher($pages, options){
             delay: _this.getDelay($page)
         };
     });
-    this.styles = this.transitions.map(function(i, trans) {
+
+    this.styles = this.transitions.map(function GetStyles(i, trans) {
         var next_trans = _this.transitions[(i + 1) % _this.transitions.length];
         console.log(trans);
-        console.log(next_trans);
+        console.log(next_trans, trans.time);
+        console.log(_this.getTransitionStyle(trans.type, trans.time, true));
         return {
             before: _this.getTransitionStyle(trans.type, trans.time, false),
             after: _this.getTransitionStyle(next_trans.type, next_trans.time, true)
         }
     });
-    this.transitions =
-        {none: function($current_item, $next, transition_time){
-            $next.addClass('active');
-            $current_item.removeClass('active');
-        },
-        fade: function($current, $next, transition_time){
-            $current.fadeOut(transition_time, function() {$current.removeClass('active')});
-            $next.fadeIn(transition_time, function (){$next.addClass('active')});
-        }};
+
 }
 
 PageSwitcher.prototype.getItem = function(index){
@@ -55,7 +60,7 @@ PageSwitcher.prototype.getDelay = function($item){
 };
 
 PageSwitcher.prototype.getTransitionTime = function($item){
-    var time = $item.attr("time");
+    var time = $item.attr("transition_time");
     if (time == null) {
         time = this.options['transition_time'];
     }
@@ -63,7 +68,7 @@ PageSwitcher.prototype.getTransitionTime = function($item){
 };
 
 PageSwitcher.prototype.getTransitionStyle = function(type, time, outfading) {
-    var halfTime = time / 2; // use half the time for fading in, half for fading out
+    var halfTime = time / 2 / 1000; // use half the time for fading in, half for fading out
     switch (type) {
         case 'swipe':
             var position = outfading ? "100%" : "-100%";
@@ -76,8 +81,10 @@ PageSwitcher.prototype.getTransitionStyle = function(type, time, outfading) {
         case 'fade':
             var transition = "opacity " + halfTime + "s";
             return {
-                opacity: "1",
-                transition: transition
+                //opacity: "1",
+                "transition": transition,
+                "-webkit-transition": transition,
+                "-moz-transition": transition
             };
             break;
         default:
@@ -87,21 +94,30 @@ PageSwitcher.prototype.getTransitionStyle = function(type, time, outfading) {
 
 PageSwitcher.prototype.next = function() {
     var current = this.current,
-        next = current + 1 % this.page_no,
-        $current = this.getItem(current),
+        next = (current + 1) % this.page_no;
+
+    console.log(current, next, this.page_no);
+    var $current = this.getItem(current),
         $next = this.getItem(next),
         currentStyle = this.styles[current].after,
         nextStyle = this.styles[next].before,
         transitionTime = this.transitions[next].time,
-        nextDelay = this.transitions[next].delay;
+        delay = this.transitions[next].delay;
+
+    console.log(currentStyle, nextStyle);
     $current.removeAttr('style').css(currentStyle);
     $next.removeAttr('style').css(nextStyle);
     $current.removeClass('active');
+    $next.addClass('active');
     window.setTimeout(function() {
-        $current.css('visibility', 'hidden');
-        $next.addClass('active')
+        $current.removeAttr('style');
     }, transitionTime/2);
-    window.setTimeout(this.next, transitionTime + nextDelay)
+
+    //delay=300;
+    //console.log(current, next, this.page_no, transitionTime, nextDelay);
+    this.current = next;
+    //this.next();
+    window.setTimeout(jQuery.proxy(this.next, this), delay)
 };
 
 PageSwitcher.prototype.run = function(){
@@ -127,19 +143,11 @@ PageSwitcher.prototype.run = function(){
     }
 
     delay = this.getDelay($next);
-    this.transitions[transition]($current, $next);
+    ///this.transitions[transition]($current, $next);
     this.in_a_transition = false;
     this.current = next;
     window.setTimeout(jQuery.proxy(this.run, this), delay);
 };
-function Iterator(item){
-    $item = $(item);
-    to_iterate = $item.attr('for').split(",");
-    if (to_iterate.length == 2) {
-
-    }
-    console.log(jQuery(this).html(), item, this);
-}
 
 
 
